9e4d888244b763f22da815f5463e678bc9e6820a
cryptite <cryptite@gmail.com>
2022-08-12 15:29:55 -0500

    1.19

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,26 @@
 plugins {
     `java-library`
     `maven-publish`
-    id("io.papermc.paperweight.userdev") version "1.3.5"
+    id("io.papermc.paperweight.userdev") version "1.3.8"
     id("xyz.jpenilla.run-paper") version "1.0.6" // Adds runServer and runMojangMappedServer tasks for testing
 }
 
 group = "com.lokamc"
-version = "2.2"
+version = "2.3"
 description = "LokaLib helpful utilities"
 
 repositories {
     mavenCentral()
+    mavenLocal()
     maven("https://maven.enginehub.org/repo/")
-    maven("https://test.lokamc.com:444/releases")
+//    maven("https://test.lokamc.com:444/releases")
 }
 
 dependencies {
-    paperweightDevBundle("com.lokamc.slice", "1.18.2-R0.1-SNAPSHOT")
+    paperweightDevBundle("com.lokamc.slice", "1.19.2-R0.1-SNAPSHOT")
     implementation("commons-io:commons-io:2.11.0")
-    implementation("com.github.ben-manes.caffeine:caffeine:3.0.5")
-    implementation("org.ocpsoft.prettytime:prettytime:5.0.2.Final")
+    implementation("com.github.ben-manes.caffeine:caffeine:3.1.1")
+    implementation("org.ocpsoft.prettytime:prettytime:5.0.3.Final")
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.6")
     compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.2.8")
 }
@@ -62,7 +63,7 @@ publishing {
     repositories {
         maven {
             name = "Ysera"
-            url = uri("https://ysera.dyndns.org:444/releases")
+            url = uri("https://test.lokamc.com:444/releases")
             // https://docs.gradle.org/current/samples/sample_publishing_credentials.html
             credentials(PasswordCredentials::class)
         }
--- src/main/java/com/lokamc/utils/SerializeInventory.java
+++ src/main/java/com/lokamc/utils/SerializeInventory.java
@@ -1,7 +1,7 @@
 package com.lokamc.utils;
 
 import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.v1_18_R2.inventory.CraftItemStack;
+import org.bukkit.craftbukkit.v1_19_R1.inventory.CraftItemStack;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.io.BukkitObjectInputStream;
